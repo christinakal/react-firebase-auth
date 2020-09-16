65233abcc10191f173af7b6d847d23d0b3277dc2
Christina Kaliora <crhstina.kaliora@pirron.one>
2020-09-16 15:46:41 +0200

    added components/images

---
+++ src/components/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <form>
+      <label>Name</label>
+      <input type="text">Hellooooo!</input>
+    </form>
+  );
+}
