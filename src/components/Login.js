86621e2772754a854a5eea1813b8e9ffe8b8dd53
Christina Kaliora <crhstina.kaliora@pirron.one>
2020-09-16 16:31:54 +0200

    basic login form is done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,20 @@
 import React from "react";
+import "./login.css";
 
 export default function Login() {
   return (
-    <form>
-      <label>Name</label>
-      <input type="text">Hellooooo!</input>
+    <form className="form">
+      {/* email */}
+      <div className="email">
+        <label className="email-label">Email</label>
+        <input type="email"></input>
+      </div>
+      {/* password */}
+      <div className="password">
+        <label className="password-label">Password</label>
+        <input type="password"></input>
+      </div>
+      <button>Login</button>
     </form>
   );
 }
