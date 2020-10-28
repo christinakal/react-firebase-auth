bef4116710015a6957916165eae3dc5c1c5bf3a3
Christina Kaliora <christina.kaliora@pirron.one>
2020-10-28 11:51:12 +0100

    centered elements in the main page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,16 +3,19 @@ import "./login.css";
 
 export default function Login() {
   return (
-    <form className="form">
-      <div className="email">
-        <label className="email-label">Email</label>
-        <input type="email"></input>
-      </div>
-      <div className="password">
-        <label className="password-label">Password</label>
-        <input type="password"></input>
-      </div>
-      <button>Login</button>
-    </form>
+    <div className="form-container">
+      <form className="form">
+        <div className="email">
+          <label className="email-label">Email</label>
+          <input type="email"></input>
+        </div>
+        <div className="password">
+          <label className="password-label">Password</label>
+          <input type="password"></input>
+        </div>
+        <button>Login</button>
+      </form>
+    </div>
   );
-}
+} 
+ 
